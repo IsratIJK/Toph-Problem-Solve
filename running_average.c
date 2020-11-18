2ab968abef513cc44d219b37dbfee585f9f81007
IsratIJK <iptu.jahan.amin@gmail.com>
2020-11-18 23:11:26 +0600

    ghjkjhg

---
+++ running_average.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int n,i,a[n];
+    double avg,sum=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+        scanf("%d",&a[i]);
+        sum+=a[i];
+        avg=sum/(i+1);
+        printf("%lf\n",avg);
+
+    }
+    return 0;
+
+}
