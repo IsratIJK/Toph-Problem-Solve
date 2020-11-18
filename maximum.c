cbdaee4b25a9b8c3b0cc555ab8c3ae6444245883
IsratIJK <iptu.jahan.amin@gmail.com>
2020-11-18 21:15:53 +0600

    gshckdjd

---
+++ maximum.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int i, t,a;
+    int max=0;
+    scanf("%d" , &t);
+    for (i=1; i<=t; i++)
+    {
+        scanf("%d" , &a);
+
+        if (a>max){
+            max=a;
+    }
+    }
+    printf("%d\n" , max);
+	return 0;
+}
